f4d7864e26a8daefd88b24f60341db19b46be94b
MacBookPro <ryotaeisaki0530@icloud.com>
2020-01-28 20:23:04 +0900

    edit

--- 2/184.c
+++ 2/184.c
@@ -4,19 +4,23 @@
 
 int main(void) {
     int length =20;
+   
     int state[length];
     double density;
+    
     int time =20;
-    double t;
+   
+   double t;
 
     //first state
     scanf("%lf", &density);
 
-    int seed=149;
-    srand(seed);
+    int seed[10] = {149,193,251,383,457,503,691,761,829,991};
+    srand(seed[5]);
 
     for (int i=0;i<length;i++){
-            t=(double)(2*rand()/(RAND_MAX + 1.0 ));
+            t=(double)(1*rand()/(RAND_MAX + 1.0 ));
+            printf("%lf\n",t);
             if (t<density){
                 state[i]=1;
             }
@@ -24,8 +28,19 @@ int main(void) {
                 state[i]=0;
             }
     }
+
+    printf("%s\n","Base State; ");
+    int test;
+    for (int k=0; k<length; k++){
+        if(state[k]==1){
+            test++;
+        }
+        printf("%d",state[k]);
+    }
+    printf("\n");
+    double value= (double)test/length;
+    printf("%f\n",value);
     
-    // printf("%f\n",);
 
     // ca184
     for (int i=0; i<time; i++){
